25bf0052ec4f4c9acd055478c3bcb4a80e67e809
DESKTOP-6N9G5LS\USUARIO <aarroyaveduque20@gmail.com>
2023-12-11 10:08:11 -0500

    Se agregan funcionalidades a los botones, lógica de juego y limpieza del tablero

--- blackjack/assets/js/game.js
+++ blackjack/assets/js/game.js
@@ -14,8 +14,11 @@ let computerPoints = 0
 
 // Referencias del HTML
 const btnOrder = document.querySelector('#btnOrder')
+const btnStop = document.querySelector('#btnStop')
+const btnNew = document.querySelector('#btnNew')
 const pointsHTML = document.querySelectorAll('small')
 const divPlayerCards = document.querySelector('#player-cards')
+const divComputerCards = document.querySelector('#computer-cards')
 
 // Crea un nuevo deck
 const createDeck = () => {
@@ -55,9 +58,79 @@ const valueCard = (card) => {
             : Number(value)
 }
 
-// Eventos
+// Turno de la computafora
+const computerShift = (miniumPoints) => {
+    do {
+        const card = orderCard()
+        computerPoints += valueCard(card)
+        pointsHTML[1].innerText = computerPoints
+
+        const imgCard = document.createElement('img')
+        imgCard.src = `assets/cartas/${card}.png`
+        imgCard.classList.add('cards')
+
+        divComputerCards.append(imgCard)
+
+        if(miniumPoints > 21) break
+    } while (computerPoints < miniumPoints && miniumPoints <= 21);
+
+    setTimeout(() => {
+        if(computerPoints === miniumPoints) {
+            alert('Nadie gana')
+        } else if(miniumPoints > 21) {
+            alert('Gana computadora')
+        } else if(computerPoints > 21) {
+            alert('Gana jugador')
+        } else alert('Gana computadora')
+    }, 100);
+}
+
+// Evento, pedir carta
 btnOrder.addEventListener('click', () => {
     const card = orderCard()
     playerPoints += valueCard(card)
     pointsHTML[0].innerText = playerPoints
+
+    const imgCard = document.createElement('img')
+    imgCard.src = `assets/cartas/${card}.png`
+    imgCard.classList.add('cards')
+
+    divPlayerCards.append(imgCard)
+
+    if(playerPoints > 21) {
+        btnOrder.disabled = true
+        btnStop.disabled = true
+        computerShift(playerPoints)
+    }
+    if(playerPoints === 21) {
+        btnOrder.disabled = true
+        btnStop.disabled = true
+        computerShift(playerPoints)
+    }
+})
+
+// Evento, detener juego
+btnStop.addEventListener('click', () => {
+    btnOrder.disabled = true
+    btnStop.disabled = true
+
+    computerShift(playerPoints)
+})
+
+// Evento, nuevo juego
+btnNew.addEventListener('click', () => {
+    deck = []
+    deck = createDeck()
+
+    btnOrder.disabled = false
+    btnStop.disabled = false
+
+    playerPoints = 0
+    computerPoints = 0
+
+    pointsHTML[0].innerText = playerPoints
+    pointsHTML[1].innerText = computerPoints
+
+    divPlayerCards.innerHTML = ''
+    divComputerCards.innerHTML = ''
 })
